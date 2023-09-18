8ed7b0723c1f8f40972b4287e38a586f7d6bcc06
Joshua Buehrer <louisproductions179@gmail.com>
2023-09-18 12:21:13 -0500

    arrays/exercises

--- arrays/exercises/part-five-arrays.js
+++ arrays/exercises/part-five-arrays.js
@@ -2,10 +2,14 @@ let str = 'In space, no one can hear you code.';
 let arr = ['B', 'n', 'n', 5];
 
 //1) Use the split method on the string to identify the purpose of the parameter inside the ().
-
+let method1 = str.split(' ')
+console.log(method1)
 //2) Use the join method on the array to identify the purpose of the parameter inside the ().
-
+let method2 = arr.join('+')
+console.log(method2)
 //3) Do split or join change the original string/array?
-
+//no
 //4) We can take a comma-separated string and convert it into a modifiable array. Try it! Alphabetize the cargoHold string, and then combine the contents into a new string.
 let cargoHold = "water,space suits,food,plasma sword,batteries";
+const newCargo = cargoHold.split(",").sort()
+console.log(newCargo)
--- arrays/exercises/part-six-arrays.js
+++ arrays/exercises/part-six-arrays.js
@@ -1,11 +1,23 @@
 //Arrays can hold different data types, even other arrays! A multi-dimensional array is one with entries that are themselves arrays.
 
 //1) Define and initialize the arrays specified in the exercise to hold the name, chemical symbol and mass for different elements.
-
+let cargoHold = [
+    ["water bottles", "meal packs", "snacks", "chocolate"],
+    ["space suits", "jet packs", "tool belts", "thermal detonators"],
+    ["parrots", "cats", "moose", "alien eggs"],
+    ["blankets", "pillows", "eyepatches", "alarm clocks"],
+]
+cargoHold[1],[2],[3].sort();
+console.log(cargoHold)
 //2) Define the array 'table', and use 'push' to add each of the element arrays to it. Print 'table' to see its structure.
-
+console.log(cargoHold.push('table'))
+console.log('table')
 //3) Use bracket notation to examine the difference between printing 'table' with one index vs. two indices (table[][]).
-
+console.log(cargoHold[3])
+console.log(cargoHold[1][3])
 //4) Using bracket notation and the table array, print the mass of element1, the name for element 2 and the symbol for element26.
-
+console.log(cargoHold[1]);
+console.log(cargoHold[2][1])
+console.log('element26: undefined')
 //5) 'table' is an example of a 2-dimensional array. The first “level” contains the element arrays, and the second level holds the name/symbol/mass values. Experiment! Create a 3-dimensional array and print out one entry from each level in the array.
+console.log(cargoHold[1][2][3])
