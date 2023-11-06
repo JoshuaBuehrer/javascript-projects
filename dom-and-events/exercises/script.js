ba9ffda7edcddf3b984642dafdc6942962c4daf1
Joshua Buehrer <louisproductions179@gmail.com>
2023-11-05 21:09:38 -0600

    DOM and events exercises

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,24 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+    });
+
+    missionAbort.addEventListener('mouseover', event => {
+        document.body.style.backgroundColor = 'red'
+    });
+
+    missionAbort.addEventListener('mouseout', event => {
+        document.body.style.backgroundColor = ''
+    });
+
+    missionAbort.addEventListener('click', event => {
+       text = window.confirm('Are you sure you want to abort the mission?');
+        if(text == true) {
+            paragraph.innerHTML = 'Mission aborted! Space shuttle returning home'
+        }
+    })
 }
 
 window.addEventListener("load", init);
